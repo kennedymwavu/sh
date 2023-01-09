ee239c3a93c20a4d84aa68bcf9ba3891771190aa
kennedymwavu <mwavukennedy@gmail.com>
2023-01-09 20:45:29 +0300

    Toggle submit btn
    If the inputs order id and price aren't truthy, disable submit btn, otherwise enable it

--- R/mod_new_order_server.R
+++ R/mod_new_order_server.R
@@ -6,6 +6,16 @@ mod_new_order_server <- function(id) {
         id = "add_order",
         print("Adding order...")
       )
+      
+      observeEvent(
+        eventExpr = c(input$order_id, input$price), 
+        handlerExpr = {
+          shinyjs::toggleState(
+            id = "add_order", 
+            condition = isTruthy(input$order_id) && isTruthy(input$price)
+          )
+        }
+      )
     }
   )
 }
