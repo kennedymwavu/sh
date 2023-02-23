2a4bb83b82c2bb98cfdfca7665c9b66ca0f9c0a9
kennedymwavu <mwavukennedy@gmail.com>
2023-02-23 20:07:38 +0300

    ft: switch tabs, leave "hole" for cards

--- ui.R
+++ ui.R
@@ -4,51 +4,44 @@ ui <- bslib::page(
   shiny::includeCSS(path = file.path("www", "styles.css")),
   shinyjs::useShinyjs(),
   shinytoastr::useToastr(),
-
   shiny::tags$body(
     class = "bg-light d-flex justify-content-center",
     shiny::tags$div(
-    class = paste(
-      "container m-3 p-5 bg-white shadow rounded",
-      "d-flex justify-content-center"
-    ),
-
-    shiny::tabsetPanel(
-      id = "tabs",
-      type = "pills",
-      header = shiny::tags$hr(),
-
-      shiny::tabPanel(
-        title = "New Order",
-        value = "new_order",
-        icon = shiny::icon(name = NULL, class = "bi bi-plus-circle"),
-        mod_new_order_ui(id = "new_order")
+      class = paste(
+        "container m-3 p-5 bg-white shadow rounded",
+        "d-flex justify-content-center"
       ),
-
-      shiny::tabPanel(
-        title = "Outstanding Orders",
-        value = "os",
-        icon = shiny::icon(name = NULL, class = "bi bi-exclamation-circle"),
-        mod_table_ui(id = "os", status = "Outstanding"),
-        mod_request_payments_ui(id = "payments")
-      ),
-
-      shiny::tabPanel(
-        title = "All Orders",
-        value = "all",
-        icon = shiny::icon(name = NULL, class = "bi bi-card-list"),
-        mod_table_ui(id = "all", status = NULL)
-      ),
-
-      shiny::tabPanel(
-        title = "Paid Orders",
-        value = "paid",
-        icon = shiny::icon(name = NULL, class = "bi bi-check2-all"),
-        mod_table_ui(id = "paid", status = "Paid")
+      shiny::tabsetPanel(
+        id = "tabs",
+        type = "pills",
+        header = shiny::tags$hr(),
+        shiny::tabPanel(
+          title = "New Order",
+          value = "new_order",
+          icon = shiny::icon(name = NULL, class = "bi bi-plus-circle"),
+          mod_new_order_ui(id = "new_order")
+        ),
+        shiny::tabPanel(
+          title = "All Orders",
+          value = "all",
+          icon = shiny::icon(name = NULL, class = "bi bi-card-list"),
+          shiny::uiOutput(outputId = "cards"),
+          mod_table_ui(id = "all", status = NULL)
+        ),
+        shiny::tabPanel(
+          title = "Outstanding Orders",
+          value = "os",
+          icon = shiny::icon(name = NULL, class = "bi bi-exclamation-circle"),
+          mod_table_ui(id = "os", status = "Outstanding"),
+          mod_request_payments_ui(id = "payments")
+        ),
+        shiny::tabPanel(
+          title = "Paid Orders",
+          value = "paid",
+          icon = shiny::icon(name = NULL, class = "bi bi-check2-all"),
+          mod_table_ui(id = "paid", status = "Paid")
+        )
       )
     )
   )
-  )
 )
-
-
