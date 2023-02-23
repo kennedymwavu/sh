a74f16343a42fd99592f8f050f25aca9d300c092
kennedymwavu <mwavukennedy@gmail.com>
2023-02-23 16:37:41 +0300

    ft: add icons to tabs

--- ui.R
+++ ui.R
@@ -21,12 +21,14 @@ ui <- bslib::page(
       shiny::tabPanel(
         title = "New Order",
         value = "new_order",
+        icon = shiny::icon(name = NULL, class = "bi bi-plus-circle"),
         mod_new_order_ui(id = "new_order")
       ),
 
       shiny::tabPanel(
         title = "Outstanding Orders",
         value = "os",
+        icon = shiny::icon(name = NULL, class = "bi bi-exclamation-circle"),
         mod_table_ui(id = "os", status = "Outstanding"),
         mod_request_payments_ui(id = "payments")
       ),
@@ -34,12 +36,14 @@ ui <- bslib::page(
       shiny::tabPanel(
         title = "All Orders",
         value = "all",
+        icon = shiny::icon(name = NULL, class = "bi bi-card-list"),
         mod_table_ui(id = "all", status = NULL)
       ),
 
       shiny::tabPanel(
         title = "Paid Orders",
         value = "paid",
+        icon = shiny::icon(name = NULL, class = "bi bi-check2-all"),
         mod_table_ui(id = "paid", status = "Paid")
       )
     )
