46cc033862cd2a0ffe09bae87b9184cf1ce289d5
kennedymwavu <mwavukennedy@gmail.com>
2023-02-23 17:55:53 +0300

    bg: add net_price before binding data.tables

--- R/mod_new_order_server.R
+++ R/mod_new_order_server.R
@@ -1,9 +1,9 @@
 mod_new_order_server <- function(id) {
   shiny::moduleServer(
-    id = id, 
+    id = id,
     module = function(input, output, session) {
       ns <- session$ns
-      
+
       shinyjs::onclick(
         id = "add_order",
         {
@@ -17,38 +17,38 @@ mod_new_order_server <- function(id) {
                   role = "status",
                   `aria-hidden` = "true"
                 ),
-                
                 "Adding..."
               )
             )
           )
-          
+
           # disable btn:
           shinyjs::disable(id = "add_order")
-          
+
           # read previous orders, bind new order:
           orders <- rbind(
             readRDS(file = "orders.rds"), # this is a data.table
             list(
               order_id = input$order_id,
               price = as.integer(input$price),
+              net_price = as.integer(input$price) * 0.65,
               deadline = lubridate::ymd_hms(
-                input$deadline, 
+                input$deadline,
                 tz = "Africa/Nairobi"
               ),
               status = input$status
             )
           )
-          
+
           # save orders:
           saveRDS(object = orders, file = "orders.rds")
-          
+
           # restore initial html on btn:
           shinyjs::html(
             id = "add_order",
             html = "Add Order"
           )
-          
+
           # reset form:
           lapply(c("order_id", "price", "status"), shinyjs::reset)
           shinyWidgets::updateAirDateInput(
@@ -56,38 +56,38 @@ mod_new_order_server <- function(id) {
             inputId = "deadline",
             clear = TRUE
           )
-          
+
           # show success:
           shinytoastr::toastr_success(
             message = paste0("Order ID ", input$order_id, " added")
           )
         }
       )
-      
+
       observeEvent(
-        eventExpr = c(input$order_id, input$price, input$deadline), 
+        eventExpr = c(input$order_id, input$price, input$deadline),
         handlerExpr = {
           # show/hide * in req fields:
           shinyjs::toggle(
-            id = "order_id_req", 
+            id = "order_id_req",
             condition = !isTruthy(input$order_id)
           )
-          
+
           shinyjs::toggle(
             id = "price_req",
             condition = !isTruthy(input$price)
           )
-          
+
           shinyjs::toggle(
             id = "deadline_req",
             condition = !isTruthy(input$deadline)
           )
-          
+
           # enable/disable `add_order` btn:
           shinyjs::toggleState(
-            id = "add_order", 
-            condition = isTruthy(input$order_id) && 
-              isTruthy(input$price) && 
+            id = "add_order",
+            condition = isTruthy(input$order_id) &&
+              isTruthy(input$price) &&
               isTruthy(input$deadline)
           )
         }
