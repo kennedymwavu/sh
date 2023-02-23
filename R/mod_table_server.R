7a30c02a43d72e7cd93f52a17952a41a1b238d1b
kennedymwavu <mwavukennedy@gmail.com>
2023-02-23 18:15:37 +0300

    ft: capitalize column headers
    ch: change pending icon

--- R/mod_table_server.R
+++ R/mod_table_server.R
@@ -2,29 +2,29 @@
 #'
 #' @param id Module id
 #' @param orders A data.table containing the orders
-#' @param status Status to be shown in the rendered table. Either "Paid" or 
+#' @param status Status to be shown in the rendered table. Either "Paid" or
 #' "Outstanding". A value of `NULL` shows all orders.
 #'
 #' @return NULL
 #' @noRd
 mod_table_server <- function(id, orders, status) {
   stopifnot("`orders` must be reactive" = is.reactive(orders))
-  
+
   envir <- environment()
   status_null <- is.null(status)
-  
+
   tbl <- reactive({
     if (status_null) {
       return(orders())
     }
-    
-    # otherwise, filter those rows and drop the status column since 
+
+    # otherwise, filter those rows and drop the status column since
     # it won't be necessary to show:
     orders()[
       data.table::`%chin%`(status, get("status", envir = envir))
     ][, .SD, .SDcols = -c("status")]
   })
-  
+
   shiny::moduleServer(
     id = id,
     module = function(input, output, session) {
@@ -33,33 +33,46 @@ mod_table_server <- function(id, orders, status) {
           tbl(),
           columns = {
             lst <- list(
-              price = reactable::colDef(cell = as_currency("$")),
-              net_price = reactable::colDef(cell = as_currency("$")),
-              status = reactable::colDef(cell = function(value) {
-                label <- switch(
-                  value,
-                  Paid = "Paid",
-                  Outstanding = "Pending"
-                )
-                
-                badge <- switch(
-                  value,
-                  Paid = tags$i(
-                    class = "bi bi-check-lg text-success pe-1"
-                  ),
-                  Outstanding = tags$i(
-                    class = "bi bi-circle-fill text-warning pe-1"
+              order_id = reactable::colDef(
+                name = "Order ID"
+              ),
+              price = reactable::colDef(
+                name = "Price",
+                cell = as_currency("$")
+              ),
+              net_price = reactable::colDef(
+                name = "Net Price",
+                cell = as_currency("$")
+              ),
+              status = reactable::colDef(
+                name = "Status",
+                cell = function(value) {
+                  label <- switch(value,
+                    Paid = "Paid",
+                    Outstanding = "Pending"
                   )
-                )
-                
-                shiny::tagList(badge, label)
-              })
+
+                  badge <- switch(value,
+                    Paid = tags$i(
+                      class = "bi bi-check-lg text-success pe-1"
+                    ),
+                    Outstanding = tags$i(
+                      class = "bi bi-exclamation-circle-fill text-warning pe-1"
+                    )
+                  )
+
+                  shiny::tagList(badge, label)
+                }
+              ),
+              deadline = reactable::colDef(
+                name = "Deadline"
+              )
             )
-            
+
             if (!status_null) {
               lst$status <- NULL
             }
-            
+
             lst
           },
           selection = if (status_null) NULL else "multiple",
@@ -69,7 +82,7 @@ mod_table_server <- function(id, orders, status) {
           wrap = FALSE
         )
       })
-      
+
       selected <- reactive({
         reactable::getReactableState(
           outputId = "table",
@@ -77,10 +90,10 @@ mod_table_server <- function(id, orders, status) {
           session = session
         )
       })
-      
+
       # hide/show `change_status` btn:
       shiny::observeEvent(
-        eventExpr = selected(), 
+        eventExpr = selected(),
         handlerExpr = {
           shinyjs::toggle(
             id = "change_status",
@@ -89,20 +102,19 @@ mod_table_server <- function(id, orders, status) {
         },
         ignoreNULL = FALSE
       )
-      
+
       # change order status:
       shiny::observeEvent(
         eventExpr = input$change_status,
         handlerExpr = {
           order_ids <- tbl()$order_id[selected()]
           orders_copy <- data.table::copy(orders())
-          new_status <- switch(
-            status,
+          new_status <- switch(status,
             Paid = "Outstanding",
             Outstanding = "Paid"
           )
           orders_copy[order_id %in% order_ids, status := new_status]
-          
+
           # update file:
           saveRDS(object = orders_copy, file = "orders.rds")
         }
